7b60956d6ae78052ab7d3cb19abc1a5c4c8b0116
Jayres18 <jaylansang07@gmail.com>
2025-10-02 15:33:27 +0800

    Mobile Menu Initilized.

--- app/components/SideNavigation.tsx
+++ app/components/SideNavigation.tsx
@@ -4,12 +4,21 @@ import React from "react";
 import Image from "next/image";
 import { menuItemsTop } from "@/contents/sidenavmenutop";
 import { menuItemsBottom } from "@/contents/sidenavmenubottom";
+import { usePathname } from "next/navigation";
+import { useState } from "react";
+import { XMarkIcon, Bars3Icon } from "@heroicons/react/24/outline";
 
 const SideNavigation = () => {
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState<boolean>(false);
+  const pathname = usePathname();
+  const toggleMobileMenu = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen);
+  };
+
   return (
-    <nav className="fixed top-0 left-0 h-full w-65 bg-slate-50 dark:bg-gray-800 border-r border-gray-200 dark:border-gray-700 p-4 rounded-lg">
+    <nav className="fixed top-0 left-0 h-full w-23 md:w-65 bg-slate-50 dark:bg-gray-800 border-r border-gray-200 dark:border-gray-700 p-4 rounded-lg">
       <div className="container pt-7">
-        <div className="flex flex-col items-center mb-5">
+        <div className="hidden md:flex flex-col items-center mb-5">
           <Link
             href="/"
             className="flex text-center text-xl items-center font-bold mb-7"
@@ -23,21 +32,30 @@ const SideNavigation = () => {
             />
             GameHub&trade;
           </Link>
+
+          {/* Desktop Menu */}
           <div className="flex flex-col gap-30 h-full w-full">
             <div className="flex flex-col mt-4 gap-6">
-              {menuItemsTop.map((itemTop) => (
-                <Link key={itemTop.href} href={itemTop.href}>
-                  <div className="flex items-center p-2 text-lg font-medium rounded-lg hover:bg-gray-200 dark:hover:bg-gray-700 transition-colors">
-                    <Image
-                      src={itemTop.image}
-                      alt={itemTop.label}
-                      width={25}
-                      height={25}
-                    />
-                    <span className="ml-3">{itemTop.label}</span>
-                  </div>
-                </Link>
-              ))}
+              {menuItemsTop.map((itemTop) => {
+                const isActive = pathname === itemTop.href;
+                return (
+                  <Link key={itemTop.href} href={itemTop.href}>
+                    <div
+                      className={`flex items-center p-2 text-lg font-medium rounded-lg hover:bg-gray-200 dark:hover:bg-gray-700 transition-colors ${
+                        isActive ? "text-primary" : ""
+                      }`}
+                    >
+                      <Image
+                        src={itemTop.image}
+                        alt={itemTop.label}
+                        width={25}
+                        height={25}
+                      />
+                      <span className="ml-3">{itemTop.label}</span>
+                    </div>
+                  </Link>
+                );
+              })}
             </div>
             <div className="flex flex-col mt-auto gap-6">
               {menuItemsBottom.map((itemBottom) => (
@@ -56,6 +74,21 @@ const SideNavigation = () => {
             </div>
           </div>
         </div>
+        <div>
+          {/* Mobile Menu Button */}
+          <button
+            onClick={toggleMobileMenu}
+            className="md:hidden  rounded-lg hover:bg-gray-200 dark:hover:bg-gray-700 transition-colors cursor-pointer"
+          >
+            {isMobileMenuOpen ? (
+              <XMarkIcon className="h-6 w-6" />
+            ) : (
+              <Bars3Icon className="h-6 w-6" />
+            )}
+          </button>
+        </div>
+
+        {/* Mobhile Menu */}
       </div>
     </nav>
   );
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { ThemeProvider } from "./components/theme-provider";
+import SideNavigation from "./components/SideNavigation";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,11 +25,19 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-white transition-colors dark:bg-gray-900 text-gray-800 dark:text-slate-200`}
       >
-        {children}
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <SideNavigation />
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
